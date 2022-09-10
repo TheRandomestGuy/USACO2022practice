20b5ecca804906ac31e64d241d4070e360167ce3
Arjun Mehan <arjun.mehan1@gmail.com>
2022-09-10 17:07:56 -0400

    lonely photo

--- Bronze/lonelyphoto.cpp
+++ Bronze/lonelyphoto.cpp
@@ -8,5 +8,15 @@ string line;
 int main(){
     cin >> N >> line;
 
-    
+    int ans = 0;
+    for(int i = 0; i < (int)line.size(); i++) {
+        int g = 0;
+        int h = 0;
+        for(int j = i; j < (int)line.size(); j++) {
+            if(line[j] == 'G') g++;
+            else h++;
+            if(g+h >= 3 && (g==1 || h==1)) ans++;
+        }
+    }
+    cout << ans;
 }
